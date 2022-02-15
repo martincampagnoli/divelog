9903a777409f14cfec46571341ae7816d889817d
Martin Campagnoli <martin.campagnoli@incentro.com>
2022-02-15 15:11:02 +0100

    Update app name

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const path = require('path');
 
 const app = express();
 
-//UPDATE project name here to finish express config
-const project = 'whip';
+const project = 'divelog';
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/' + project));
